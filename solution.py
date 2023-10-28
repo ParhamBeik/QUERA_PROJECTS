694597d39fe3b8a4ea59fc1ef94ddb6057602aeb
Parham <parhambeik2183@gmail.com>
2023-10-28 22:56:00 +0330

    making changes for the better

--- solution.py
+++ solution.py
@@ -1,13 +1,16 @@
 def calculator(n, m, li):
     newList=[]
     addedNumbers=0
-    numberItertedCount=1
-    indexCount=0
-    while numberItertedCount!=m and len(li)!=0:
-        for _ in range(m):
-            addedNumbers+=li[indexCount]
+    if len(li)>=m:
+        for number in range(m):
+            addedNumbers+=li[number]
         newList.append(addedNumbers)
+        for number in range(m):
+            li.pop(number)
         addedNumbers=0
-        numberItertedCount+=1
+    else:
+        for number in range(len(li)):
+            addedNumbers+=li[number]
+        newList.append(addedNumbers)
     print(newList)
 calculator(8,3,[1,2,3,4,5,6,7,8])
