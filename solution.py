15274e0bfa83a50660cb9192fa7ca9041da40321
Parham <parhambeik2183@gmail.com>
2023-10-28 22:33:41 +0330

    making project "M taei" work

--- solution.py
+++ solution.py
@@ -1,23 +1,16 @@
 def calculator(n, m, li):
     newList=[]
-    addednumbers=0
-    indexNumber=0
-    if type(n/m)==int:
-        indexNumber=n//m
-    else:
-        indexNumber=n//m+1
-    for _ in range(indexNumber):
-        if len(li)>=3:
-            for number in range(m):
-                addednumbers+=li[number]
-                li.pop(number)
-            newList.append(addednumbers)
-            addednumbers=0
-        else:
-            for number in range(len(li)-1):
-                addednumbers+=li[number]
-                li.pop(number)
-            newList.append(addednumbers)
-            addednumbers=0
+    addedNumbers=0
+    numberItertedCount=1
+    start=0
+    finish=3
+    while numberItertedCount!=m and len(li)!=0:
+        for number in li[start:finish:]:
+            addedNumbers+=number
+        start+=m
+        finish+=m
+        newList.append(addedNumbers)
+        addedNumbers=0
+        numberItertedCount+=1
     print(newList)
 calculator(8,3,[1,2,3,4,5,6,7,8])
