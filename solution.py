04941962d2b066b51ff750a1369459778e4fb94a
Parham <parhambeik2183@gmail.com>
2023-10-30 15:39:13 +0330

    reWriting the code for quera project

--- solution.py
+++ solution.py
@@ -1,16 +1,7 @@
 def calculator(n, m, li):
     newList=[]
+    numbersThatShouldBeSummed=[]
     addedNumbers=0
-    if len(li)>=m:
-        for number in range(m):
-            addedNumbers+=li[number]
-        newList.append(addedNumbers)
-        for number in range(m):
-            li.pop(number)
-        addedNumbers=0
-    else:
-        for number in range(len(li)):
-            addedNumbers+=li[number]
-        newList.append(addedNumbers)
+    
     print(newList)
 calculator(8,3,[1,2,3,4,5,6,7,8])
