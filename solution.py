d7a6b7a72dd6999c9bb4c9571234ae260bc4d95c
Parham <parhambeik2183@gmail.com>
2023-10-30 16:11:09 +0330

    making progress of the quera project

--- solution.py
+++ solution.py
@@ -2,6 +2,18 @@ def calculator(n, m, li):
     newList=[]
     numbersThatShouldBeSummed=[]
     addedNumbers=0
-    
+    indexCount=0
+    while indexCount<len(li):
+        while len(numbersThatShouldBeSummed)<3:
+            try:
+                numbersThatShouldBeSummed.append(li[indexCount])
+            except IndexError:
+                break
+            indexCount+=1
+        for number in numbersThatShouldBeSummed:
+            addedNumbers+=number
+        newList.append(addedNumbers)
+        addedNumbers=0
+        numbersThatShouldBeSummed.clear()
     print(newList)
 calculator(8,3,[1,2,3,4,5,6,7,8])
