dd05b54f3b37babca2065e75afc7ef9bf6be530e
Parham <parhambeik2183@gmail.com>
2023-11-02 10:34:53 +0330

    making 9th project work

---
+++ UNIVERSITY/file4.py
@@ -0,0 +1,26 @@
+firstNumber=int(input())
+secondNumber=int(input())
+listOfFirstOddNumbers=[]
+listOfSecondOddNumbers=[]
+
+variable=10
+while variable<=firstNumber*10:
+    listOfFirstOddNumbers.append(firstNumber%variable - firstNumber%(variable//10))
+    variable*=10
+variable=10
+while variable<=secondNumber*10:
+    listOfSecondOddNumbers.append(secondNumber%variable - secondNumber%(variable//10))
+    variable*=10
+
+indexCorrection=100
+
+for index in range(0,len(listOfSecondOddNumbers),2):
+    listOfFirstOddNumbers[index]*=indexCorrection
+    listOfSecondOddNumbers[index]*=indexCorrection
+    
+for index in range(2,len(listOfFirstOddNumbers),2):
+    listOfFirstOddNumbers[index]=listOfFirstOddNumbers[index]//indexCorrection
+    listOfSecondOddNumbers[index]=listOfSecondOddNumbers[index]//indexCorrection
+    
+print(listOfFirstOddNumbers)
+print(listOfSecondOddNumbers)
