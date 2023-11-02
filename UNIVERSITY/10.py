69ea5e2104ac58ca46763230ae5c114c015b6872
Parham <parhambeik2183@gmail.com>
2023-11-02 20:34:00 +0330

    finishing 10th project

--- UNIVERSITY/10.py
+++ UNIVERSITY/10.py
@@ -5,13 +5,15 @@ base=int(input())
 listOfNumbers=[number for number in range(startingNumber+1,finishingNumber)]
 sumOfTheDigitsOfNumbers=[]
 
+listOfDigits=[]
 for number in range(startingNumber+1,finishingNumber):
     sumOfDigits=0
     while number!=1:
-        sumOfDigits+=(number%base)
+        listOfDigits.append(number%base)
         number//base
+    for digit in listOfDigits:
+        sumOfDigits+=digit
     sumOfTheDigitsOfNumbers.append(sumOfDigits)
-
-
+    listOfDigits.clear()
     
 print(listOfNumbers[sumOfTheDigitsOfNumbers.index(max(sumOfTheDigitsOfNumbers))])
