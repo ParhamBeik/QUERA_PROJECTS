cd33bd9f1d4c321c86d9b2466d8eb1f077889765
Parham Beik <parhambeik2183@gmail.com>
2023-11-02 23:41:29 +0330

    almost complete

--- UNIVERSITY/8 Project.cpp
+++ UNIVERSITY/8 Project.cpp
@@ -1,21 +1,41 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int startingNumber;
-    int finishingNumber;
+    long int startingNumber;
+    long int finishingNumber;
     cin >> startingNumber;
     cin >> finishingNumber;
-
-    if (startingNumber % 2 == 0 && finishingNumber % 2 == 0) {
-        int totalSum = 1;
-        for (int i = 0; i < (finishingNumber - startingNumber) / 2; i++) {
-            totalSum *= 2;
+    long long int numberOfEven = 0;
+    long long int numberOfOdd = 0;
+    for (int number = startingNumber; number <= finishingNumber; number++) {
+        if (number % 2 == 0) {
+            numberOfEven += 1;
+        } else {
+            numberOfOdd += 1;
         }
-        cout << totalSum << endl;
     }
-    else if ((startingNumber % 2 == 0 && finishingNumber % 2 == 1) || (startingNumber % 2 == 1 && finishingNumber % 2 == 0)) {
-        cout << 0 << endl;
+    long long int totalSumOdd = 1;
+    long long int totalSumEven = 1;
+    if (startingNumber % 2 == 0 && finishingNumber % 2 == 0) {
+        for (int i = 0; i < numberOfEven; i++) {
+            totalSumEven *= 2;
+        }
+        for (int i = 0; i < numberOfOdd; i++) {
+            totalSumOdd *= 2;
+        }
+        cout << totalSumEven - totalSumOdd ;
+    } else if (startingNumber % 2 == 1 && finishingNumber % 2 == 1) {
+        for (int i = 0; i < numberOfEven; i++) {
+            totalSumEven *= 2;
+        }
+        for (int i = 0; i < numberOfOdd; i++) {
+            totalSumOdd *= 2;
+        }
+        cout << totalSumOdd - totalSumEven ;
+    } else if ((startingNumber % 2 == 0 && finishingNumber % 2 == 1) || (startingNumber % 2 == 1 && finishingNumber % 2 == 0)) {
+        cout << 0 ;
     }
     return 0;
 }
+
 
--- UNIVERSITY/8.py
+++ UNIVERSITY/8.py
@@ -1,16 +1,30 @@
 startingNumber=int(input())
 finishingNumber=int(input())
 numberOfEven=0
+numberOfOdd=0
 
 for number in range(startingNumber,finishingNumber+1):
     if number%2==0:
         numberOfEven+=1
-        
+    else:
+        numberOfOdd+=1
+
+totalSumOdd=1
+totalSumEven=1
+
 if startingNumber%2==0 and finishingNumber%2==0:
-    totalSum=1
     for _ in range(numberOfEven):
-        totalSum*=2
-    print(totalSum)
+        totalSumEven*=2
+    for _ in range(numberOfOdd):
+        totalSumOdd*=2
+    print(totalSumEven-totalSumOdd)
+    
+elif startingNumber%2==1 and finishingNumber%2==1:
+    for _ in range(numberOfEven):
+        totalSumEven*=2
+    for _ in range(numberOfOdd):
+        totalSumOdd*=2
+    print(totalSumOdd-totalSumEven)
     
 elif (startingNumber%2==0 and finishingNumber%2==1) or (startingNumber%2==1 and finishingNumber%2==0):
     print(0)
