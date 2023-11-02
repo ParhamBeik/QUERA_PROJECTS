d670167a7a47e3cc6c610df3b9d3dc4ad652910d
Parham Beik <parhambeik2183@gmail.com>
2023-11-02 23:15:27 +0330

    finishing 8th project

--- UNIVERSITY/8.py
+++ UNIVERSITY/8.py
@@ -1,9 +1,14 @@
 startingNumber=int(input())
 finishingNumber=int(input())
+numberOfEven=0
 
+for number in range(startingNumber,finishingNumber+1):
+    if number%2==0:
+        numberOfEven+=1
+        
 if startingNumber%2==0 and finishingNumber%2==0:
     totalSum=1
-    for _ in range((finishingNumber-startingNumber)//2):
+    for _ in range(numberOfEven):
         totalSum*=2
     print(totalSum)
     
