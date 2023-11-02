eb04d4cf8cff831aa9b3164705aa11ec8bdc78b5
Parham <parhambeik2183@gmail.com>
2023-11-02 16:32:30 +0330

    7th file changes

--- UNIVERSITY/7.py
+++ UNIVERSITY/7.py
@@ -1,12 +1,15 @@
 listOfNumbers=[]
 number=int(input())
-listOfNumbers.append(number)
-newNumber=number
-while newNumber!=-1:
-    newNumber=int(input())
-    if newNumber!=-1:
-        listOfNumbers.append(newNumber)
-totalSum=0
-for number in listOfNumbers:
-    totalSum+=number
-print(totalSum)
+if number!=-1:
+    listOfNumbers.append(number)
+    newNumber=number
+    while newNumber!=-1:
+        newNumber=int(input())
+        if newNumber!=-1:
+            listOfNumbers.append(newNumber)
+    totalSum=0
+    for number in listOfNumbers:
+        totalSum+=number
+    print(totalSum)
+else:
+    print(0)
