e56a8d52b153117d2562ce60dc47adc6935e1c8c
Parham <parhambeik2183@gmail.com>
2023-11-04 23:22:37 +0330

    finishing some projects

---
+++ UNIVERSITY/10.py
@@ -0,0 +1,7 @@
+#get the numbers
+startingNumber=int(input())
+finishingNumber=int(input())
+base=int(input())
+
+# turn them into the base required one by one as they go along and add each digit to a total sum
+# 
--- UNIVERSITY/8 Project.cpp
+++ UNIVERSITY/8 Project.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int startingNumber;
-    int finishingNumber;
+    long long int startingNumber;
+    long long int finishingNumber;
     cin >> startingNumber;
     cin >> finishingNumber;
-    int numberOfOddNumbers = 0;
-    int numberOfEvenNumbers = 0;
+    long long int numberOfOddNumbers = 0;
+    long long int numberOfEvenNumbers = 0;
     for (int number = startingNumber; number <= finishingNumber; number++) {
         if (number % 2 == 0) {
             numberOfEvenNumbers += 1;
@@ -14,15 +14,15 @@ int main() {
             numberOfOddNumbers += 1;
         }
     }
-    int totalSum = 1;
-    if ((startingNumber % 2 == 0 && finishingNumber % 2 == 1) || (startingNumber % 2 == 1 && finishingNumber % 2 == 0)) {
+    long long int totalSum = 1;
+    if ((startingNumber % 2 == 0 && finishingNumber % 2 != 0) || (startingNumber % 2 != 0 && finishingNumber % 2 == 0)) {
         cout << 0 ;
     } else if (startingNumber % 2 == 0 && finishingNumber % 2 == 0) {
-        for (int index = 0; index < numberOfOddNumbers; index++) {
+        for (long long int index = 0; index < numberOfOddNumbers; index++) {
             totalSum *= 2;
         }
-    } else if (startingNumber % 2 == 1 && finishingNumber % 2 == 1) {
-        for (int index = 0; index < numberOfEvenNumbers; index++) {
+    } else if (startingNumber % 2 != 0 && finishingNumber % 2 != 0) {
+        for (long long int index = 0; index < numberOfEvenNumbers; index++) {
             totalSum *= 2;
         }
     }
