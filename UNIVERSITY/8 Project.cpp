ee136aea73e5bfdf844487b0f0668d89ce9a1292
Parham <parhambeik2183@gmail.com>
2023-11-05 22:49:23 +0330

    changing file

--- UNIVERSITY/8 Project.cpp
+++ UNIVERSITY/8 Project.cpp
@@ -1,33 +1,29 @@
 #include <iostream>
+#include<cmath>
 using namespace std;
 int main() {
-    long long int startingNumber;
-    long long int finishingNumber;
+    int startingNumber,finishingNumber;
+
     cin >> startingNumber;
     cin >> finishingNumber;
-    long long int numberOfOddNumbers = 0;
-    long long int numberOfEvenNumbers = 0;
-    for (int number = startingNumber; number <= finishingNumber; number++) {
-        if (number % 2 == 0) {
-            numberOfEvenNumbers += 1;
-        } else {
-            numberOfOddNumbers += 1;
-        }
-    }
+
+    int exponantial=(finishingNumber-startingNumber)/2;
+
     long long int totalSum = 1;
-    if ((startingNumber % 2 == 0 && finishingNumber % 2 != 0) || (startingNumber % 2 != 0 && finishingNumber % 2 == 0)) {
-        cout << 0 ;
-    } else if (startingNumber % 2 == 0 && finishingNumber % 2 == 0) {
-        for (long long int index = 0; index < numberOfOddNumbers; index++) {
-            totalSum *= 2;
+    if(startingNumber!=finishingNumber){
+        if ((startingNumber % 2 == 0 && finishingNumber % 2 != 0) || (startingNumber % 2 != 0 && finishingNumber % 2 == 0)) {
+            cout << 0 ;
+        } else if (startingNumber % 2 == 0 && finishingNumber % 2 == 0) {
+            totalSum=pow(2,exponantial);
+        } else if (startingNumber % 2 != 0 && finishingNumber % 2 != 0) {
+            totalSum=pow(2,exponantial);
         }
-    } else if (startingNumber % 2 != 0 && finishingNumber % 2 != 0) {
-        for (long long int index = 0; index < numberOfEvenNumbers; index++) {
-            totalSum *= 2;
+        if (totalSum != 1) {
+            cout << totalSum ;
         }
     }
-    if (totalSum != 1) {
-        cout << totalSum ;
+    else{
+        cout<<1;
     }
     return 0;
 }
