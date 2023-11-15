f1a507397a8493861cf1cf1ff14f0a092508388e
Parham Beik <parhambeik2183@gmail.com>
2023-11-15 22:54:42 +0330

    making some progress into the third project

--- UNIVERSITY 3/Project 2.cpp
+++ UNIVERSITY 3/Project 2.cpp
@@ -1,6 +1,5 @@
 #include<iostream>
 #include<cmath>
-#include<iomanip>
 using namespace std;
 float distance(int round){
     int akbarX=1,akbarY=1,asgharX=-1,asgharY=1;
@@ -21,9 +20,9 @@ float distance(int round){
     else{
         akbarX=3,akbarY=2,asgharX=-2,asgharY=2;
         float distanceOfTwo=0.00;
-        akbarX+=(int)(round/2)*1;
-        akbarY+=(int)(round/2)*2;
-        asgharX-=(int)(round/2)*2;
+        akbarX+=(int)((round-2)/2)*1;
+        akbarY+=(int)((round-2)/2)*2;
+        asgharX-=(int)((round-2)/2)*2;
         distanceOfTwo=(float)sqrt((akbarX-asgharX)*(akbarX-asgharX)+(akbarY-asgharY)*(akbarY-asgharY));
         return distanceOfTwo;
     }
@@ -33,6 +32,7 @@ int main(){
     float resault;
     cin>>numberOfRound;
     resault=distance(numberOfRound);
-    cout << setprecision(4) <<(float)(resault);
+    cout.precision(2);
+    cout<<fixed<<resault;
     return 0;
 }
--- UNIVERSITY 3/Project 3.cpp
+++ UNIVERSITY 3/Project 3.cpp
@@ -1,12 +1,28 @@
-#include<iostream>
-#include<algorithm> 
-#include<string>
+#include <iostream>
+#include <string>
+using namespace std;
 
+void ReverseFiboNumbers(string number){
 
+}
 
-int main(){
-    string stringOfNumbers = (str)(number); 
-    reverse(stringOfNumbers.begin(), stringOfNumbers.end()); 
+void MakingPrimeNumbers(int numberOfRounds) {
+    int firstNum = 0;
+    int secondNum = 1;
+    for (int i = 0; i < numberOfRounds; i++) {
+        int thirdNum = firstNum + secondNum;
+        firstNum = secondNum;
+        secondNum = thirdNum;
+        ReverseFiboNumbers(thirdNum);
+    }
+}
 
+int main() {
+    int rounds;
+    cin >> rounds;
+    cout << 0 << " "<< 1 << " ";
+    MakingPrimeNumbers(rounds - 2);
     return 0;
 }
+
+
