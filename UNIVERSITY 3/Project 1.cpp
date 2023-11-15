6db36bc38972c8abe778040e98327fb0dd4f8b72
Parham <parhambeik2183@gmail.com>
2023-11-15 13:53:26 +0330

    putting some into motion

--- UNIVERSITY 3/Project 1.cpp
+++ UNIVERSITY 3/Project 1.cpp
@@ -1,7 +1,22 @@
 #include<iostream>
 using namespace std;
-int main(){
 
-    
+void primeNumbers(int number){
+	int count=0;
+	for(int i=2;i<=number;i++){
+		for(int x=2;x<=i;x++)
+			if(i%x==0 && i!=x){
+				count++;
+			}
+		if(count==0){
+			cout<<i;
+		}
+	}
+}
+
+int main(){
+	int number;
+	cin>>number;
+	primeNumbers(number);
     return 0;
 }
