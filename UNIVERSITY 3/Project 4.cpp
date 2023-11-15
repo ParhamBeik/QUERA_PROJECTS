3a5ac4103734f2387cbcb69b40e4f4b376fdbe85
Parham Beik <parhambeik2183@gmail.com>
2023-11-15 23:58:07 +0330

    saving some changes

---
+++ UNIVERSITY 3/Project 4.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+int KMM(int first,int second){
+    int kmm=1,copyOfSecond=second;
+    for(int x=2;x<copyOfSecond;x++){
+        while(second%x==0 && first%x==0){
+            kmm*=x;
+            second/=x;
+            first/=x;
+        }        
+    }
+    return kmm;
+}
+
+void BMM(int first,int second){
+    int kmm=KMM(first,second),bmm,difference ;
+    bmm=first*second/kmm;
+    difference=(bmm-kmm)*2;
+    if(first!=second){
+        cout<<difference<<endl;
+    }
+    else{
+        cout<<0<<endl;
+    }
+}
+
+int main(){
+    int rounds,firstNum,secondNum;
+    cin>>rounds;
+    for(int i=0;i<rounds;i++){            
+        cin>>firstNum>>secondNum;
+        BMM(firstNum,secondNum);
+    }  
+    return 0;
+}
