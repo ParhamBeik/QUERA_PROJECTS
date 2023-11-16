04faf3628b206fcb69e60dbf8be770aaf268c13d
Parham Beik <parhambeik2183@gmail.com>
2023-11-16 19:15:55 +0330

    finishing up some projects

--- UNIVERSITY 3/Project 7.cpp
+++ UNIVERSITY 3/Project 7.cpp
@@ -1,34 +1,48 @@
 #include<iostream>
 using namespace std;
-int main(){
-    int timeLimit,forwardStep,backStep,startPosition,finishPosition,totalPosition;
-    cin >> timeLimit >> forwardStep >> backStep >> startPosition >> finishPosition >> totalPosition;
 
+int Jolo(int timeLimit,int forwardStep,int backStep,int startPosition,int finishPosition,int totalPosition){
     int forwardResault,backwardResault;
-
     if(startPosition<finishPosition){
         forwardResault=timeLimit-((finishPosition-startPosition)*forwardStep);
     }
     else{
         forwardResault=timeLimit-((totalPosition-startPosition+finishPosition)*forwardStep);
     }
-
     if(startPosition<finishPosition){
         backwardResault=timeLimit-((startPosition+totalPosition-finishPosition)*backStep);
     }
     else{
         backwardResault=timeLimit-((startPosition-finishPosition)*backStep);
     }
-    
-
     if(forwardResault>=0){
         cout<<"J : "<<forwardResault<<endl;
-    }
-    if(backwardResault>=0){
-        cout<<"A : "<<backwardResault<<endl;
     }
     if(forwardResault<0 && backwardResault<0){
         cout<<-1;
     }
+    return 0;
+}
+
+int Aghab(int timeLimit,int backStep,int startPosition,int finishPosition,int totalPosition){
+    int backwardResault;
+    if(startPosition<finishPosition){
+        backwardResault=timeLimit-((startPosition+totalPosition-finishPosition)*backStep);
+    }
+    else{
+        backwardResault=timeLimit-((startPosition-finishPosition)*backStep);
+    }
+    if(backwardResault>=0){
+        cout<<"A : "<<backwardResault<<endl;
+    }
+    return 0;
+}
+int main(){
+    int timeLimit,forwardStep,backStep,startPosition,finishPosition,totalPosition;
+    cin >> timeLimit >> forwardStep >> backStep >> startPosition >> finishPosition >> totalPosition;
+
+    Jolo(timeLimit,forwardStep,backStep,startPosition,finishPosition,totalPosition);
+    Aghab(timeLimit,backStep,startPosition,finishPosition,totalPosition);
+
     return 0;
 }
