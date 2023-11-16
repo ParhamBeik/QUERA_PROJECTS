05115861db0086fbb3eba6f88144dc3a0df98fba
Parham Beik <parhambeik2183@gmail.com>
2023-11-16 10:07:07 +0330

    starting the 5th project

---
+++ UNIVERSITY 3/Project 5.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+double CalculateVolume(double radius){
+
+}
+
+double CalculateVolume(double length,double width,double height){
+    
+}
+
+double CalculateVolume(double radius,double height){
+    
+}
+
+int main(){
+    return 0;
+}
