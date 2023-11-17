cf56921656d0d2f3ca33582dec898f97e2f4e7c7
Parham Beik <parhambeik2183@gmail.com>
2023-11-17 13:07:59 +0330

    making some changes to the code

--- UNIVERSITY 3/Project 6.cpp
+++ UNIVERSITY 3/Project 6.cpp
@@ -2,41 +2,62 @@
 #include<math.h>
 using namespace std;
 
-int Tavan(long long int n){
-    long long int numPow=pow(2,n);
-    return numPow+1;
-}
-
-bool isPrime(long long int number){
-}
-
-int TheResault(long long int n){
-    long long int number=Tavan(n),numberOfPrimeMaghsoom=0;
-    for(long long int i=2;i<=number;i++){
-        if(number%i==0 && isPrime(i)){
-            numberOfPrimeMaghsoom++;
-        }
+int Tavan(int n){
+    int numberSquered=1;
+    for(int i=0;i<n;i++){
+        numberSquered*=2;
     }
-    while(numberOfPrimeMaghsoom%3!=0){
-        numberOfPrimeMaghsoom=0;
-        for(long long int i=2;i<=number;i++){
-            if(number%i==0 && isPrime(i)){
-                numberOfPrimeMaghsoom++;
+    return numberSquered+1;
+}
+
+// bool isPrime(int number){
+//     int numberSqrt=sqrt(number)+1;
+//     bool isNumPrime=true;
+//     for(int i=2;i<numberSqrt;i++){
+//         if(number%i==0 || number==1){
+//             isNumPrime=false;
+//         }
+//     }
+//     if(isNumPrime){
+//         return true;
+//     }
+//     else{
+//         return false;
+//     }
+// }
+
+int Resault(int n){
+    int number=Tavan(n);
+    int copyOfNumber=number,PrimeFactorCount=0,index=2;
+    while(copyOfNumber!=1){
+        while(copyOfNumber%index==0){
+            copyOfNumber=copyOfNumber/index;
+        }
+        PrimeFactorCount++;
+        index++;
+    }
+    if(PrimeFactorCount%3==0){
+        return number;
+    }
+    else{
+        while(PrimeFactorCount%3!=0){
+            number++;
+            copyOfNumber=number,PrimeFactorCount=0,index=2;
+            while(copyOfNumber!=1){
+                while(copyOfNumber%index==0){
+                    copyOfNumber=copyOfNumber/index;
+                }
+                PrimeFactorCount++;
+                index++;
             }
         }
-        if(numberOfPrimeMaghsoom%3==0){
-            return number;
-        }
-        else{
-            number++;
-        }
+        return number;
     }
-    return number;
 }
 
 int main(){
     int n;
     cin>>n;
-    cout<<TheResault(n);
+    cout<<Resault(n);
     return 0;
 }
