6593269d7e41f41d6aa36124dc70e3c637e82334
Parham <parhambeik2183@gmail.com>
2023-11-20 00:43:34 +0330

    finishing another project

--- Jadval Zarb Gonde.py
+++ Jadval Zarb Gonde.py
@@ -2,13 +2,15 @@ n=int(input())
 round=0
 index=0
 step=1
-while(round<n)
-		start=1
-		start+=round
-		while(index<n):
-			print(start,end=" ")
-			start+=step
-			index+=1
-		print()
-		step+=1
-		round+=1
+while round<n:
+    start=1
+    start+=round
+    index=0
+    while index<n:
+        print(start,end=" ")
+        start+=step
+        index+=1
+    if index==n:
+        print()
+    step+=1
+    round+=1
