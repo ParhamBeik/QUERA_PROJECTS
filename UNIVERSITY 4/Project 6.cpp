0cdea68a7869bc48e49404669dbeeac691cb8211
Parham <parhambeik2183@gmail.com>
2023-12-01 10:15:35 +0330

    finishing UNIVERSITY 4

--- UNIVERSITY 4/Project 6.cpp
+++ UNIVERSITY 4/Project 6.cpp
@@ -1,22 +1,40 @@
 #include<iostream>
 using namespace std;
 int main(){
-	int num,step,number;
-	cin>>num;
-	int index=(num*(num+1))/2;
+	int n,index,step=0,number=1;
+	cin>>n;
+	index=(4*n-3)/2;
 	
-	number=1;
-	step=0;
-	
-	for(int i=1;i<=num;i++){
-		for(int j=1;j<=num*(num+1)-1;j++){
-			
+	for(int i=0;i<n;i++){
+		for(int j=0;j<(4*n-3);j++){
+			if( (j>=index-step-number+1 && j<=index-step) || (j<=index+step+number-1 && j>=index+step)){
+				cout<<(number%10);
+			}
+			else{
+				cout<<" ";
+			}
 		}
-		step++;
 		number++;
+		step++;
 		cout<<endl;
 	}
 	
-
+	step=n-2;
+	number=n-1;
+	
+	for(int i=0;i<n-1;i++){
+		for(int j=0;j<(4*n-3);j++){
+			if( (j>=index-step-number+1 && j<=index-step) || (j<=index+step+number-1 && j>=index+step)){
+				cout<<(number%10);
+			}
+			else{
+				cout<<" ";
+			}
+		}
+		number--;
+		step--;
+		cout<<endl;
+	}
+	
 	return 0;
 }
