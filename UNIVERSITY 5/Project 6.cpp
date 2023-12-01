c74900a4524d3de06dbba0427520580d6ec0486c
Parham Beik <parhambeik2183@gmail.com>
2023-12-01 14:06:23 +0330

    adding comments to project 6 / UNI 5

--- UNIVERSITY 5/Project 6.cpp
+++ UNIVERSITY 5/Project 6.cpp
@@ -5,20 +5,23 @@ using namespace std;
 int main(){
 	int first,second;
 	vector<double> list;
+	//initializing first ladder
 	cin>>first;
 	for(int i=0;i<first;i++){
 		double num;
 		cin>>num;
 		list.push_back(num);
 	}
+	//initializing second ladder
 	cin>>second;
 	for(int i=0;i<second;i++){
 		double num;
 		cin>>num;
 		list.push_back(num);
 	}
+	//putting all the heights together or in a sense sticking to ladder together
 	sort(list.begin(), list.end());
-	
+	//assigning the high and low of the the two ladders
 	double high=0,low=0;
 	for(int i=0;i<first+second-1;i++){
 		if(i==0){
@@ -32,7 +35,7 @@ int main(){
 			low=list[i+1]-list[i];
 		}
 	}
-	
+	//printing the low and the high differenciation of the ladders
 	cout<<low<<" "<<high;
 	return 0;
 }
