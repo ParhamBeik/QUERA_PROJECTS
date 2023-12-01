6191d93400f750efc06a51f070c7c1c8f47d187d
Parham <parhambeik2183@gmail.com>
2023-12-01 15:13:38 +0330

    starting and finishing the 8th project

--- UNIVERSITY 5/Project 8.cpp
+++ UNIVERSITY 5/Project 8.cpp
@@ -1,7 +1,33 @@
 #include<iostream>
 using namespace std;
 int main(){
-
-    
+    int n;
+    cin>>n;
+    int a[n];
+    a[0]=0;
+    for(int i=1;i<n;i++){
+        bool isInList=false;
+        int test=a[i-1]-i;
+        for(int j=0;j<=i;j++){
+            if(test==a[j]){
+                isInList=true;
+            }
+        }
+        if(test>0 && isInList==false){
+            a[i]=test;
+        }
+        else{
+            a[i]=a[i-1]+i;
+        }
+    }
+    for(int i=0;i<n;i++){
+        int div=10,total=0,copy=a[i];
+        while(copy!=0){
+        	total+=copy%div;
+        	copy/=div;
+		}
+		a[i]=total;
+		cout<<a[i]<<", ";
+    }
     return 0;
 }
