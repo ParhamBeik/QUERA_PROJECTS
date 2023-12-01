322f1daf53f6c8e9e96559ae1637bc11e476876b
Parham Beik <parhambeik2183@gmail.com>
2023-12-01 13:59:09 +0330

    finishing the 9th project from PROJECT UNI 5

--- UNIVERSITY 5/Project 9.cpp
+++ UNIVERSITY 5/Project 9.cpp
@@ -8,8 +8,14 @@ int main(){
 		list[i]=i+1;
 	}
 	
-	int index=0,step=1;
-	for(int i=0;i<n-1;i++){
+	int index=0,step=1,count;
+	count=0;
+	for(int i=0;i<n;i++){
+		if(list[i]!=0){
+			count++;
+		}
+	}
+	while(count!=1){
 		if(list[step%n]!=0){
 			if((index)%2==0){
 				list[step%n]=0;
@@ -24,9 +30,17 @@ int main(){
 		else{
 			step++;
 		}
+		count=0;
+		for(int i=0;i<n;i++){
+			if(list[i]!=0){
+				count++;
+			}
+		}
 	}
 	for(int i=0;i<n;i++){
-		cout<<list[i]<<" ";
+		if(list[i]!=0){
+			cout<<list[i];
+		}
 	}
 	return 0;
 }
