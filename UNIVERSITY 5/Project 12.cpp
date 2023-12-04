2fe4baa1b3fddb9f9eec4f6eac67567f40c98159
Parham <parhambeik2183@gmail.com>
2023-12-04 20:38:24 +0330

    starting some projects

--- UNIVERSITY 5/Project 12.cpp
+++ UNIVERSITY 5/Project 12.cpp
@@ -1,7 +1,12 @@
 #include<iostream>
 using namespace std;
 int main(){
-
+    char a[25];
+    for(int i=0;i<25;i++){
+        char number;
+        cin>>number;
+        a[i]=number;
+    }
     
     return 0;
 }
