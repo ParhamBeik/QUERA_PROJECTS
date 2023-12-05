abe02783e6ff2fcc3d7330eecd0a835c55e3e6a4
Parham <parhambeik2183@gmail.com>
2023-12-05 19:42:56 +0330

    finishing the 12th project almost

--- UNIVERSITY 5/Project 12.cpp
+++ UNIVERSITY 5/Project 12.cpp
@@ -11,63 +11,107 @@ int main(){
             a[i][j]=character;
         }
     }
+
+    vector<int> list;
+
     string res="S";
     int count=0;
     int i=0,j=0;
+
     while(res!="E"){
         //going forward
-        if(j+1>0 && j+1<5 && a[i][j+1]=="."){
+        isTrue1=true;
+        for(int a=0;a<list.size();a++){
+            if(a%2==0 && list[a]==i && list[a+1]==j){
+                isTrue1=false;
+            }
+        }
+        if(j+1>0 && j+1<5 && a[i][j+1]=="." && isTrue1){
             res=a[i][j+1];
             count++;
+            list.push_back(i);
+            list.push_back(j);
             j++;
         }
-        else if(j+1>0 && j+1<5 && a[i][j+1]=="E"){
+        else if(j+1>0 && j+1<5 && a[i][j+1]=="E" && isTrue1){
             res=a[i][j+1];
             count++;
+            list.push_back(i);
+            list.push_back(j);
             j++;
             cout<<count;
             break;
         }
         //going downward
-        else if(i-1>0 && i-1<5 && a[i-1][j]=="."){
+        isTrue1=true;
+        for(int a=0;a<list.size();a++){
+            if(a%2==0 && list[a]==i && list[a+1]==j){
+                isTrue1=false;
+            }
+        }
+        else if(i-1>0 && i-1<5 && a[i-1][j]=="." && isTrue1){
             res=a[i-1][j];
             count++;
+            list.push_back(i);
+            list.push_back(j);
             i--;
         }
-        else if(i-1>0 && i-1<5 && a[i-1][j]=="E"){
+        else if(i-1>0 && i-1<5 && a[i-1][j]=="E" && isTrue1){
             res=a[i-1][j];
             count++;
+            list.push_back(i);
+            list.push_back(j);
             i--;
             cout<<count;
             break;
         }
         //going leftwards
-        else if(j-1>0 && j-1<5 && a[i][j-1]=="."){
+        isTrue1=true;
+        for(int a=0;a<list.size();a++){
+            if(a%2==0 && list[a]==i && list[a+1]==j){
+                isTrue1=false;
+            }
+        }
+    
+        else if(j-1>0 && j-1<5 && a[i][j-1]=="." && isTrue1){
             res=a[i][j-1];
             count++;
+            list.push_back(i);
+            list.push_back(j);
             j--;
         }
-        else if(j-1>0 && j-1<5 && a[i][j-1]=="E"){
+        else if(j-1>0 && j-1<5 && a[i][j-1]=="E" && isTrue1){
             res=a[i][j-1];
             count++;
+            list.push_back(i);
+            list.push_back(j);
             j--;
             cout<<count;
             break;
         }
         //going upwards
-        else if(i+1>0 && i+1<5 && a[i+1][j]=="."){
+        isTrue1=true;
+        for(int a=0;a<list.size();a++){
+            if(a%2==0 && list[a]==i && list[a+1]==j){
+                isTrue1=false;
+            }
+        }
+        else if(i+1>0 && i+1<5 && a[i+1][j]=="." && isTrue1){
             res=a[i+1][j];
             count++;
+            list.push_back(i);
+            list.push_back(j);
             i++;
         }
-        else if(i+1>0 && i+1<5 && a[i+1][j]=="E"){
+        else if(i+1>0 && i+1<5 && a[i+1][j]=="E" && isTrue1){
             res=a[i+1][j];
             count++;
+            list.push_back(i);
+            list.push_back(j);
             i++;
             cout<<count;
             break;
         }
-        
         else if(a[i][j]=="."){
             cout<<"Lost!";
             break;
