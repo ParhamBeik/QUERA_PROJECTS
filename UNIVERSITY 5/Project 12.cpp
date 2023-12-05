f4416d7c7e01e82c150cabdee8b84ce36c6be881
Parham <parhambeik2183@gmail.com>
2023-12-05 18:53:26 +0330

    making some changes to the 12th project

--- UNIVERSITY 5/Project 12.cpp
+++ UNIVERSITY 5/Project 12.cpp
@@ -1,42 +1,73 @@
 #include<iostream>
+#include<vector>
+#include<string>
 using namespace std;
 int main(){
-    char a[5][5];
+    string a[5][5];
     for(int i=0;i<5;i++){
         for(int j=0;j<5;j++){
-            char character;
+            string character;
             cin>>character;
             a[i][j]=character;
         }
     }
-    char res="S";
+    string res="S";
     int count=0;
     int i=0,j=0;
     while(res!="E"){
+        //going forward
         if(j+1>0 && j+1<5 && a[i][j+1]=="."){
             res=a[i][j+1];
             count++;
             j++;
         }
+        else if(j+1>0 && j+1<5 && a[i][j+1]=="E"){
+            res=a[i][j+1];
+            count++;
+            j++;
+            cout<<count;
+            break;
+        }
+        //going downward
         else if(i-1>0 && i-1<5 && a[i-1][j]=="."){
             res=a[i-1][j];
             count++;
             i--;
         }
+        else if(i-1>0 && i-1<5 && a[i-1][j]=="E"){
+            res=a[i-1][j];
+            count++;
+            i--;
+            cout<<count;
+            break;
+        }
+        //going leftwards
         else if(j-1>0 && j-1<5 && a[i][j-1]=="."){
             res=a[i][j-1];
             count++;
             j--;
         }
+        else if(j-1>0 && j-1<5 && a[i][j-1]=="E"){
+            res=a[i][j-1];
+            count++;
+            j--;
+            cout<<count;
+            break;
+        }
+        //going upwards
         else if(i+1>0 && i+1<5 && a[i+1][j]=="."){
             res=a[i+1][j];
             count++;
             i++;
         }
-        else if(a[i][j]=="E"){
+        else if(i+1>0 && i+1<5 && a[i+1][j]=="E"){
+            res=a[i+1][j];
+            count++;
+            i++;
             cout<<count;
             break;
         }
+        
         else if(a[i][j]=="."){
             cout<<"Lost!";
             break;
