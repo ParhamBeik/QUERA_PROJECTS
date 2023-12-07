85ffc47a4430b7cddd007e4077e73d457c277ea5
Parham <parhambeik2183@gmail.com>
2023-12-07 21:56:11 +0330

    modifying as best as i can

--- UNIVERSITY 5/Project 10.cpp
+++ UNIVERSITY 5/Project 10.cpp
@@ -12,60 +12,70 @@ int main(){
         list.push_back(pow(2,number));
         number++;
     }
-    int i=0;
-    while(number>=2){
-    	if(i==0){
-    		number=number-2;
-		}
-        list.push_back(pow(2,number));
-        number--;
-        i++;
-    }
+    
     //making the shape for one
     if(num==1){   
         cout<<"*"<<endl<<"* *"<<endl<<"  *";
     }
-    //making the shape for the rest
+    //making the shape for the rest2
     else{   
         cout<<"*"<<endl<<"* *"<<endl<<"  *"<<endl;
+		
+		int number=1,index=0;
+		
+		for(int x=0;x<list.size();x++){
+			for(int y=0;y<list[index];y++){
+				cout<<"* ";
+			}
+			cout<<endl;
 
-        int number=1,index=0;
-        for(int i=0;i<(num-1)*2-1;i++){
-        	//printing the lines
-        	int a=0;
-            for(int y=0;y<list[index];y++){
-                if(index<=num/2){
-                	cout<<"* ";
+		    if(x!=list.size()-1){
+		    	for(int j=0;j<4*number;j++){
+			    	cout<<" ";
+			    }
+			    cout<<"*"<<endl;
+			    for(int z=0;z<4*number;z++){
+					cout<<" ";
+			    }
+			    cout<<"* *"<<endl;
+			    for(int x=0;x<4*number;x++){
+			    	cout<<" ";
 				}
-				else if(index>num/2){
-					if(a==0){
-						for(int j=0;j<4*(number-1);j++){
-		                cout<<" ";
-		            	}
-					}
-		            cout<<"* ";
-		            a++;
-				}
-            }
-            
-            cout<<endl;
-            //printing out the rest of the shapes after the first one and the lines
-            for(int j=0;j<4*number;j++){
-                cout<<" ";
-            }
-            cout<<"*"<<endl;
-            for(int z=0;z<4*number;z++){
-                cout<<" ";
-            }
-            cout<<"* *"<<endl;
-            for(int x=0;x<4*number;x++){
-                cout<<" ";
-            }
-            cout<<"  *"<<endl;
-            
-            index++;
-            number++;
-        }
+			    cout<<"  *"<<endl;
+			}
+		    number++;
+		    index++;
+    	}
+//    	number=1,index=0;
+//		
+//		for(int x=0;x<list.size();x++){
+//			
+//			for(int j=0;j<4*number;j++){
+//				cout<<" ";
+//			}
+//			cout<<"*"<<endl;
+//			for(int z=0;z<4*number;z++){
+//				cout<<" ";
+//			}
+//			cout<<"* *"<<endl;
+//			for(int x=0;x<4*number;x++){
+//				cout<<" ";
+//			}
+//			cout<<"  *"<<endl;
+//			
+//			if(x!=list.size()-1){
+//				for(int j=0;j<pow(2,number+2);j++){
+//					cout<<" ";
+//				}
+//				for(int y=0;y<list[index];y++){
+//					cout<<"* ";
+//				}
+//			}
+//			cout<<endl;
+//		    number++;
+//		    index++;
+//    	}
+//    	
     }
     return 0;
 }
--- UNIVERSITY 5/Project 12.cpp
+++ UNIVERSITY 5/Project 12.cpp
@@ -12,57 +12,71 @@ int main(){
     }
 
     string res="S";
+    bool flag[5][5];
+    for(int i=0;i<5;i++){
+        for(int j=0;j<5;j++){
+            flag[i][j]=false;
+        }
+    }
     int count=0;
     int i=0,j=0;
 
     while(res!="E"){
         //going forward
-        if(j+1>0 && j+1<5 && a[i][j+1]=="."){
+        if(j+1>0 && j+1<5 && a[i][j+1]=="." && flag[i][j+1]==false){
             res=a[i][j+1];
+            flag[i][j]=true;
             count++;
             j++;
         }
-        else if(j+1>0 && j+1<5 && a[i][j+1]=="E"){
+        else if(j+1>0 && j+1<5 && a[i][j+1]=="E" && flag[i][j+1]==false){
             res=a[i][j+1];
+            flag[i][j]=true;
             count++;
             j++;
             cout<<count;
             break;
         }
         //going downward
-        else if(i-1>0 && i-1<5 && a[i-1][j]=="."){
+        else if(i-1>0 && i-1<5 && a[i-1][j]=="." && flag[i-1][j]==false){
             res=a[i-1][j];
+            flag[i][j]=true;
             count++;
             i--;
         }
-        else if(i-1>0 && i-1<5 && a[i-1][j]=="E"){
+        else if(i-1>0 && i-1<5 && a[i-1][j]=="E" && flag[i-1][j]==false){
             res=a[i-1][j];
+            flag[i][j]=true;
             count++;
             i--;
             cout<<count;
             break;
         }
         //going leftwards
-        else if(j-1>0 && j-1<5 && a[i][j-1]=="."){
+        else if(j-1>0 && j-1<5 && a[i][j-1]=="." && flag[i][j-1]==false){
             res=a[i][j-1];
+            flag[i][j]=true;
             count++;
             j--;
         }
-        else if(j-1>0 && j-1<5 && a[i][j-1]=="E"){
+        else if(j-1>0 && j-1<5 && a[i][j-1]=="E" && flag[i][j-1]==false){
             res=a[i][j-1];
+            flag[i][j]=true;
             count++;
             j--;
             cout<<count;
             break;
         }
         //going upwards
-        else if(i+1>0 && i+1<5 && a[i+1][j]=="."){
+        else if(i+1>0 && i+1<5 && a[i+1][j]=="." && flag[i+1][j]==false){
             res=a[i+1][j];
+            flag[i][j]=true;
             count++;
             i++;
         }
-        else if(i+1>0 && i+1<5 && a[i+1][j]=="E"){
+        else if(i+1>0 && i+1<5 && a[i+1][j]=="E" && flag[i+1][j]==false){
             res=a[i+1][j];
+            flag[i][j]=true;
             count++;
             i++;
             cout<<count;
