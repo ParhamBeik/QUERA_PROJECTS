ccb04061018d01c51c3a9316159b0367e80f1b0c
Parham Beik <parhambeik2183@gmail.com>
2023-12-09 23:56:54 +0330

    writing the first project

--- UNIVERSITY 6/Project 1.cpp
+++ UNIVERSITY 6/Project 1.cpp
@@ -2,6 +2,28 @@
 #include<string>
 using namespace std;
 int main(){
-
+    string str1,str2;
+    cin >> str1 >> str2;
+    int i=0;
+    bool isSame=true;
+    while(isSame){
+        if( (str2[i]==str1[i]-32 || str1[i]==str2[i]-32 || str1[i]==str2[i]) && i<str1.size()){
+            continue;
+        }
+        else if(i==str1.size()-1){
+            break;
+        }
+        else{
+            isSame=false;
+            break;
+        }
+        i++;
+    }
+    if(isSame){
+        cout << "YES";
+    }
+    else{
+        cout << "NO";
+    }
     return 0;
 }
