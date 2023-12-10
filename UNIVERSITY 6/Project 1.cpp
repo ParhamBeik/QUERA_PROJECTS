a001bccec7e568f82d0e4c45dcda1b44937d3374
Parham <parhambeik2183@gmail.com>
2023-12-10 19:37:48 +0330

    finishing the first project

--- UNIVERSITY 6/Project 1.cpp
+++ UNIVERSITY 6/Project 1.cpp
@@ -6,20 +6,25 @@ int main(){
     cin >> str1 >> str2;
     int size=str1.size();
     
-    bool isSame=true;
-    for(int i=0;i<size;i++){
-    	if(str2[i]==str1[i]-32 || str1[i]==str2[i]-32){
-    		continue;
-		}
-		else{
-			isSame=false;
-		}
-	}
-    if(isSame){
-        cout << "YES";
+    if(size==str2.size()){
+        bool isSame=true;
+        for(int i=0;i<size;i++){
+            if(str2[i]==str1[i]-32 || str1[i]==str2[i]-32){
+                continue;
+            }
+            else{
+                isSame=false;
+            }
+        }
+        if(isSame){
+            cout << "YES";
+        }
+        else{
+            cout << "NO";
+        }
     }
     else{
-        cout << "NO";
+        cout<<"NO";
     }
     return 0;
 }
