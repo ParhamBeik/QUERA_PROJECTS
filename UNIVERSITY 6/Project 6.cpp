5a8e861022e3d163a381dac65c20ea0a78106113
Parham <parhambeik2183@gmail.com>
2023-12-13 10:12:28 +0330

    fixing project 6

--- UNIVERSITY 6/Project 6.cpp
+++ UNIVERSITY 6/Project 6.cpp
@@ -1,37 +1,41 @@
 #include<iostream>
 #include<string>
 #include<sstream>
+
 using namespace std;
 int main(){
 	//number of rounds
     int round;
     cin>>round;
+    
     //initializing the false or true value at the end
     bool isTrue[round];
     for(int i=0;i<round;i++){
     	isTrue[i]=false;
 	}
 	//going through each string and doing the calculations
-    for(int i=0;i<round;i++){
+    for(int i=0; i < round; i++){
         string str;
         cin>>str;
         
         string first="";
         string second="";
         string res="";
+
         //setting the first number
         int index=0;
-        while(str[index]!=42 || str[index]!=43 || str[index]!=45 || str[index]!=47){
+        while(isdigit(str[index])){
             first+=str[index];
             index++;
         }
+        
         //setting the operator
         string operate="";
         operate+=str[index];
         
         index++;
         //setting the second number
-        while(str[index]!=61){
+        while(isdigit(str[index])){
             second+=str[index];
             index++;
         }
@@ -39,36 +43,32 @@ int main(){
         //setting the resault value that has been given and checking if it is true or false
         while(index < str.size()){
             res+=str[index];
+            index++;
         }
-        cout<<first<<endl<<second<<res;
+        
         //turning the strings into numbers
         int firstNum,secondNum,resault,test=0;
 
-        stringstream ss;
-        ss << first;
-        ss >> firstNum;
-        
-        ss << second;
-        ss >> secondNum;
+        firstNum = stoi(first);
+        secondNum = stoi(second);
+        resault = stoi(res);
 
-        ss << res;
-        ss >> resault;
 		//doing the operations in the correct way using the integer value of them
         if(operate=="*"){
-            test=firstNum*secondNum;
+            test = firstNum * secondNum;
         }
         else if(operate=="+"){
             test=firstNum+secondNum;
         }
         else if(operate=="-"){
-            test=firstNum-secondNum;
+            test = firstNum - secondNum;
         }
         else if(operate=="/"){
-            test=firstNum/secondNum;
+            test = firstNum / secondNum;
         }
         //checking if they are the same
         if(test==resault){
-            isTrue[i]==true;
+            isTrue[i]=true;
         }
     }
 	//returning the truthiness or the falsiness of the equiations
