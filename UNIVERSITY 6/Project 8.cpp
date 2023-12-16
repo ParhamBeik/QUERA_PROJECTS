bd8b88e011c9ff7cb8c906b628372b6e1edf4764
Parham <parhambeik2183@gmail.com>
2023-12-17 00:29:07 +0330

    finishing project 8

--- UNIVERSITY 6/Project 8.cpp
+++ UNIVERSITY 6/Project 8.cpp
@@ -1,74 +1,48 @@
 #include<iostream>
+#include<vector>
 #include<string>
 using namespace std;
-int main(){
+int main(void){
     string str;
     cin>>str;
-    int list[str.size()],count1=0,count3=0;
-    for(int i=0;i<str.size();i++){
-        // F
-        if(str[i]==70){
-            list[i]=1;
-            count1++;
+    vector<char> list;
+    int i=0;
+    bool isValid=true;
+    while(i!=str.size()-1){
+        //F
+        if(str[i]=='F'){
+            list.push_back(str[i]);
         }
-        // W
-        else if(str[i]==87){
-            list[i]=2;
+        //I
+        else if(str[i]=='I'){
+            list.push_back(str[i]);
         }
-        // I
-        else if(str[i]==73){
-            list[i]=3;
-            count3++;
-        }
-        // E
-        else if(str[i]==69){
-            list[i]=4;
-        }
-    }
-	
-	bool check[str.size()-1];
-	for(int i=0;i<str.size()-1;i++){
-		check[i]=false;
-	}
-	
-    for(int i=0;i<str.size();i++){
-        int num=list[i];
-        for(int j=i+1;j<str.size();j++){
-            if(num==1){
-                if(list[j]==2){
-                    check[i]=true;
-                    break;
-                }
-                else if(list[j]==1){
-                    check[i]=false;
-                    break;
-                }
+        //W
+        else if(str[i]=='W'){
+            if(list[list.size()-1]!='F'){
+                isValid=false;
+                break;
             }
-            else if(num==3){
-                if(list[j]==4){
-                    check[i]=true;
-                    break;
-                }
-                else if(list[j]==3){
-                    check[i]=false;
-                    break;
-                }
-            }
+            list.pop_back();
         }
+        //E
+        else if(str[i]=='E'){
+            if(list[list.size()-1]!='I'){
+                isValid=false;
+                break;
+            }
+            list.pop_back();
+        }
+        
+        i++;
     }
 
-    int sum=0;
-    for(int i=0;i<str.size()-1;i++){
-        if(check[i]){
-            sum++;
-        }
-    }
-
-    if(sum==count1+count3){
+    if(isValid){
         cout<<"compile shod";
     }
     else{
         cout<<"syntax error";
     }
+
     return 0;
 }
