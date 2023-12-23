fd66ab1c19a1acfd9850ad5d151c6b835a8d8b94
Parham <parhambeik2183@gmail.com>
2023-12-23 12:41:56 +0330

    getting the rest of the points needed for the BP assignment

--- UNIVERSITY 8/Project 5.cpp
+++ UNIVERSITY 8/Project 5.cpp
@@ -1,13 +1,58 @@
 #include<iostream>
 #include<string>
 #include<sstream>
+#include<cmath>
+#include<vector>
 using namespace std;
+//Returning the Max and the Min
+int getMin(int list[],int size){
+    int min=list[0];
+    for(int i=0;i<size;i++){
+        if(list[i]<min){
+            min=list[i];
+        }
+    }
+    return min;
+}
+int getMax(int list[],int size){
+    int max=list[0];
+    for(int i=0;i<size;i++){
+        if(list[i]>max){
+            max=list[i];
+        }
+    }
+    return max;
+}
+//Functions for finding the BMM for the array
+int getGCD(int a,int b){
+    int resault=min(a,b);
+    while(resault > 0){
+        if(a%resault==0 && b%resault==0){
+            break;
+        }
+        resault--;
+    }
+    return resault;
+}
+//Function for finding the KMM for the array
+long long gcd(long long int a,long long int b){
+    if(b==0){
+        return a;
+    }
+    return gcd(b,a%b);
+}
+
+long long lcm(int a,int b){
+    return ( a / gcd(a,b)) * b;
+}
+//Main code
 int main(){
+    //Getting the number of strings
     int round;
     cin>>round;
     string str[round];
     int list[round];
-    
+    //Getting the reverse order of the strings of integers
     for(int i=0;i<round;i++){
         string var,res="";
         cin>>var;
@@ -21,7 +66,7 @@ int main(){
         }
         str[i]=res;
     }
-
+    //Turning those string of integers to the data type int
     for(int i=0;i<round;i++){
         stringstream ss;
         ss << str[i];
@@ -29,8 +74,15 @@ int main(){
         ss >> number;
         list[i]=number;
     }
-
-	
+    //Getting the Max and Min of the Array
+    int max=getMax(list,round);
+    int min=getMin(list,round);
+    //Finding the list of BMM for the numbers in the array
+    int BMM=getGCD(max,min);
+    //Finding the KMM for the numbers in the array
+    int KMM=lcm(max,min);
+    //Printing out the values of BMM and KMM
+    cout << BMM << " " << KMM;
 
     return 0;
 }
---
+++ UNIVERSITY 8/Project 6.cpp
@@ -0,0 +1,73 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<sstream>
+using namespace std;
+int main(){
+    int round;
+    cin>>round;
+    vector<int> list;
+
+    bool isValid[round];
+    for(int i=0;i<round;i++){
+        isValid[i]=false;
+    }
+
+    for(int i=0;i<round;i++){
+        for(int j=0;j<3;j++){
+            string str;
+            getline(cin >> ws, str);
+            string first="",second="";
+            bool isTrue=false;
+            for(int z=0;z<str.size();z++){
+                if(str[z]==45){
+                    isTrue=true;
+                }
+                else if(str[z]>47 && str[z]<58 && isTrue){
+                    second+=str[z];
+                }
+                else if(str[z]>47 && str[z]<58 && !(isTrue)){
+                    first+=str[z];
+                }
+            }
+            int firstNum,secondNum;
+            stringstream aa;
+            aa << first;
+            aa >> firstNum;
+            stringstream bb;
+            bb << second;
+            bb >> secondNum;
+            for(int i=firstNum;i<=secondNum;i++){
+                list.push_back(i);
+            }
+        }
+        bool isTrue=true;
+        for(int k=15;k<=24;k++){
+            int a=0;
+            for(int w=0;w<list.size();w++){
+                if(list[w]==k){
+                    a++;
+                    break;
+                }
+            }
+            if(a==0){
+                isTrue=false;
+                break;
+            }
+        }
+        if(!(isTrue)){
+            isValid[i]=true;
+        }
+    }
+    
+    for(int i=0;i<round;i++){
+        if(isValid[i]){
+            cout<<"Yes"<<endl;
+        }
+        else{
+            cout<<"No"<<endl;
+        }
+    }
+    
+    return 0;
+}
